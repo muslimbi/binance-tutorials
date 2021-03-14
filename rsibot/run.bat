e15ee3ed94e5c7298577a036780a087939a80933
Deen Islam <muslimbi@users.noreply.github.com>
2021-03-14 18:14:26 +0600

    Update run.bat

--- rsibot/run.bat
+++ rsibot/run.bat
@@ -7,3 +7,25 @@ rem venv\scripts\python.exe -m pip install --upgrade pip
 python bot.py
 
 pause
+
+=(F2-N2)/(M2-N2)*100
+
+
+https://api.binance.com/api/v1/ticker/24hr
+https://api.binance.com/api/v3/ticker/24hr
+
+https://api.binance.com/api/v1/klines?symbol=ATOMBTC&interval=1d&limit=1
+https://api.binance.com/api/v1/klines?symbol=LTCBTC&interval=1d&limit=1
+
+https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT
+https://api.binance.com/api/v1/ticker/price?symbol=LTCBTC
+
+https://api.binance.com/api/v1/ticker/price?symbol=LTCBTC
+
+https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=30m
+
+
+
+
+
+
